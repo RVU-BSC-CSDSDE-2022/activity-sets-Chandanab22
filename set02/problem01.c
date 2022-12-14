4f0a7b451e0e1c503c86263d379c00a2a12ce14e
Chandana  B <112924690+Chandanab22@users.noreply.github.com>
2022-12-14 06:29:24 +0000

    solved

---
+++ set02/problem01.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+  void input(float base, float height);
+void find_area(float base , float height, float *area);
+void output(float base, float height, float area);
+
+  int main(){
+  float base, height, area;
+  input(base, height);
+  return 0;
+  }
+  void input(float base, float height){
+  printf("entre base and height:";)
+  scanf("&f &f", &base, &height);
+  float_area(base,height,&area);
+  output(base,height,area);
+  }
+
+  void find_area(float base , float height, float *area){
+    *find_area=((base*height)/2;
+  }
+  
+void output(float base, float height, float area){
+printf("the area of a triangle %2.1f and height %2.1f" base,height,area)
+  }
+
+  
+  
+  
+
+
+  
+  
+
+
+
+ 
+
+
